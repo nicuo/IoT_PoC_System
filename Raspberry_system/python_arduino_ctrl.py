fbdb570bb48c8cf6f227188a062d5f867e95dba4
nicuo <nicuo.grape@gmail.com>
2017-08-20 22:12:49 +0900

    modify file

--- Raspberry_system/python_arduino_ctrl.py
+++ Raspberry_system/python_arduino_ctrl.py
@@ -5,12 +5,12 @@ import time
 from urllib import urlopen
 
 def main ():
-    # def main : serial communication, per 2s , count 100, print
+    # def main : serial communication, per 1s
     ser = serial.Serial('/dev/ttyACM0', 9600)
     while (True):
         time.sleep(1)
-        tmp = ser.readline()
-        print tmp
+        temp = ser.readline()
+        print temp
         urlopen('http://192.168.2.101:8080/input_temp?temp=' + str(temp))
     ser.close()
 
