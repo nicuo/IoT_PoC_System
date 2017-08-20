613657676c4416d7c4504f09bdca3a2b4827020b
nicuo <nicuo.grape@gmail.com>
2017-08-20 21:40:31 +0900

    modify file

--- Raspberry_system/python_arduino_ctrl.py
+++ Raspberry_system/python_arduino_ctrl.py
@@ -7,7 +7,7 @@ def main ():
     # def main : serial communication, per 2s , count 100, print
     ser = serial.Serial('/dev/ttyACM0', 9600)
     while (True):
-        time.sleep(2)
+        time.sleep(1)
         tmp = ser.readline()
         print tmp
     ser.close()
