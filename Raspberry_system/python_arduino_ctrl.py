b3fa874877b5e6fb5327d9886e9932df5bc1089d
nicuo <nicuo.grape@gmail.com>
2017-08-20 22:09:57 +0900

    modify file

--- Raspberry_system/python_arduino_ctrl.py
+++ Raspberry_system/python_arduino_ctrl.py
@@ -11,7 +11,7 @@ def main ():
         time.sleep(1)
         tmp = ser.readline()
         print tmp
-        urlopen('http://192.168.2.101:8080/input_temp?temp=' + str(temperture))
+        urlopen('http://192.168.2.101:8080/input_temp?temp=' + str(temp))
     ser.close()
 
 if __name__ == '__main__':
