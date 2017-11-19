b7aa2fbc102e4967d80804aa9182c7ea2e33112e
Nicuo <nicuo@>
2017-11-19 16:34:37 +0900

    modify test

--- Raspberry_system/python_arduino_ctrl.py
+++ Raspberry_system/python_arduino_ctrl.py
@@ -9,16 +9,16 @@ def test_temp():
 
 def main ():
     # def main : serial communication, per 1s
-    ser = serial.Serial('/dev/ttyACM0', 9600)
+    # ser = serial.Serial('/dev/ttyACM0', 9600)
     while (True):
         time.sleep(1)
         #temp = ser.readline()
         temp = test_temp()
         print temp
-        respose = urlopen('http://192.168.2.101:8080/input_temp?temp=' + str(temp))
-        data = response.read()
-        print (data)
-    ser.close()
+        #respose = urlopen('http://192.168.2.101:8080/input_temp?temp=' + str(temp))
+        #data = response.read()
+        #print (data)
+    #ser.close()
 
 if __name__ == '__main__':
     main()
