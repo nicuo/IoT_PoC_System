a29e534d6b774624882f6c3a29ef0ac7030c7ce3
nicuo <nicuo.grape@gmail.com>
2017-11-19 16:22:25 +0900

    add test func

--- Raspberry_system/python_arduino_ctrl.py
+++ Raspberry_system/python_arduino_ctrl.py
@@ -4,12 +4,16 @@ import serial
 import time
 from urllib import urlopen
 
+def test_temp():
+    return 26.50
+
 def main ():
     # def main : serial communication, per 1s
     ser = serial.Serial('/dev/ttyACM0', 9600)
     while (True):
         time.sleep(1)
-        temp = ser.readline()
+        #temp = ser.readline()
+        temp = test_temp()
         print temp
         respose = urlopen('http://192.168.2.101:8080/input_temp?temp=' + str(temp))
         data = response.read()
