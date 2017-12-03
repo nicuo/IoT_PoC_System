7ae9bc08938afd36a9531ce5e6b09bdbe72f1fac
Nicuo <nicuo@>
2017-12-03 20:15:18 +0900

    add except process

--- Raspberry_system/python_arduino_ctrl.py
+++ Raspberry_system/python_arduino_ctrl.py
@@ -10,15 +10,19 @@ def test_temp():
 def main ():
     # def main : serial communication, per 1s
     ser = serial.Serial('/dev/ttyACM0', 9600)
-    while (True):
-        time.sleep(1)
-        temp = ser.readline()
-        #temp = test_temp()
-        print('temerature: {}'.format(temp).rstrip())
-        response = urlopen('http://192.168.2.103:8080/input_temp?device_id=1&temperature='+ str(temp))
-        data = response.read()
-        print('response: {}'.format(data))
-    ser.close()
+    try:
+        while (True):
+            time.sleep(1)
+            temp = ser.readline()
+            #temp = test_temp()
+            print('temerature: {}'.format(temp).rstrip())
+            response = urlopen('http://192.168.2.103:8080/input_temp?device_id=1&temperature='+ str(temp))
+            data = response.read()
+            print('response: {}'.format(data))
+
+    except KeyboardInterrupt:
+        ser.close()
+        print("\nGood bye")
 
 if __name__ == '__main__':
     main()
