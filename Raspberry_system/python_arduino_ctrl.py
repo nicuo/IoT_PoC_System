d79ce79b574a394497dcfc49cb9b0d3a8bb619bb
Nicuo <nicuo@>
2017-12-03 20:07:21 +0900

    modify file

--- Raspberry_system/python_arduino_ctrl.py
+++ Raspberry_system/python_arduino_ctrl.py
@@ -9,16 +9,16 @@ def test_temp():
 
 def main ():
     # def main : serial communication, per 1s
-    # ser = serial.Serial('/dev/ttyACM0', 9600)
+    ser = serial.Serial('/dev/ttyACM0', 9600)
     while (True):
         time.sleep(1)
-        #temp = ser.readline()
-        temp = test_temp()
-        print('temerature: {}'.format(temp))
+        temp = ser.readline()
+        #temp = test_temp()
+        print('temerature: {}'.format(temp).rstrip())
         response = urlopen('http://192.168.2.103:8080/input_temp?device_id=1&temperature='+ str(temp))
         data = response.read()
         print('response: {}'.format(data))
-    #ser.close()
+    ser.close()
 
 if __name__ == '__main__':
     main()
